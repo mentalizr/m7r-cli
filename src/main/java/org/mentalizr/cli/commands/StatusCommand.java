d74e3de2e8d8bc81a9d5c3ed1cdb30fd2514773a
Arthur Picht <picht@arthurpicht.de>
2020-11-12 10:13:55 +0100

    build dependency to m7r-service-objects, SessionStatus

--- src/main/java/org/mentalizr/cli/commands/StatusCommand.java
+++ src/main/java/org/mentalizr/cli/commands/StatusCommand.java
@@ -1,13 +1,13 @@
 package org.mentalizr.cli.commands;
 
 import org.mentalizr.cli.CliContext;
-import org.mentalizr.cli.serviceObject.SessionStatus;
 import org.mentalizr.client.ClientContext;
 import org.mentalizr.client.restService.RestService;
 import org.mentalizr.client.restService.SessionStatusService;
 import org.mentalizr.client.restServiceCaller.RestServiceCaller;
 import org.mentalizr.client.restServiceCaller.exception.RestServiceConnectionException;
 import org.mentalizr.client.restServiceCaller.exception.RestServiceHttpException;
+import org.mentalizr.serviceObjects.SessionStatus;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
@@ -31,10 +31,11 @@ public class StatusCommand extends CommandExecutor {
 
         System.out.println("[OK] Status");
         System.out.println("Server " + this.cliContext.getCliConfiguration().getServer() + " is up and running.");
-        if (sessionStatus.isHasSession()) {
+        if (sessionStatus.isValid()) {
             System.out.println("Logged in as " + sessionStatus.getUserRole());
         } else {
-            System.out.println("Logged out.");
+            System.out.println("Session is logged out.");
         }
     }
+
 }
